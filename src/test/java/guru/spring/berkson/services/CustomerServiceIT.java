60f187b04682343990479b2cd903ff3ae1a86648
Berkson <berksonx@yahoo.com.br>
2021-11-24 09:27:05 -0300

    Teste de integração melhorado. Somente usando DataJpaTest. Closes #13

--- src/test/java/guru/spring/berkson/services/CustomerServiceIT.java
+++ src/test/java/guru/spring/berkson/services/CustomerServiceIT.java
@@ -1,11 +1,14 @@
 package guru.spring.berkson.services;
 
+import guru.spring.berkson.api.v1.mapper.CustomerMapper;
 import guru.spring.berkson.api.v1.model.CustomerDTO;
 import guru.spring.berkson.api.v1.model.MetaDTO;
+import guru.spring.berkson.repositories.CustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -16,22 +19,25 @@ import static org.junit.jupiter.api.Assertions.*;
  * Date: 22/11/2021
  * Time: 21:00
  */
-@SpringBootTest
+@DataJpaTest
 @ExtendWith(SpringExtension.class)
 public class CustomerServiceIT {
     private static final String FIRSTNAME = "John";
     private static final String apiURI = "/api/v1/customers/id/";
 
-    @Autowired
     CustomerService customerService;
-
+    CustomerMapper customerMapper;
+    @Autowired
+    CustomerRepository customerRepository;
     CustomerDTO client;
 
     @BeforeEach
     void setUp() {
+        customerMapper = CustomerMapper.INSTANCE;
         MetaDTO newMeta = new MetaDTO(10, 6, 5,
                 "/url/teste/h", "/url/teste/z");
         client = new CustomerDTO(newMeta, FIRSTNAME, "Jacusi", apiURI);
+        customerService = new CustomerServiceImpl(customerMapper, customerRepository);
     }
 
     @Test
@@ -41,5 +47,6 @@ public class CustomerServiceIT {
         assertNotNull(newCustomerDTO);
         assertEquals(FIRSTNAME, newCustomerDTO.getFirstname());
         assertNotEquals(apiURI, newCustomerDTO.getCustomerUrl());
+        assertEquals(10, newCustomerDTO.getMeta().getCount());
     }
 }
