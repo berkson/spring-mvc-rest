1168dc7ff11792b61ebc86128c94938f6835cd42
Berkson <berksonx@yahoo.com.br>
2021-12-01 14:18:57 -0300

    anotação produces é opcional. Produzindo json e xml de acordo com a solicitação do cliente. Closes #30

--- src/main/java/guru/spring/berkson/api/v1/controllers/CustomerController.java
+++ src/main/java/guru/spring/berkson/api/v1/controllers/CustomerController.java
@@ -7,7 +7,6 @@ import guru.spring.berkson.config.SwaggerConfig;
 import guru.spring.berkson.services.CustomerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
-@Api(tags = {SwaggerConfig.CUSTOMER_TAG}, produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(tags = {SwaggerConfig.CUSTOMER_TAG})
 public class CustomerController {
 
     public static final String BASE_URL = "/api/v1/customers";
@@ -32,21 +31,23 @@ public class CustomerController {
     }
 
     @ApiOperation(value = "Retorna uma lista de clientes", notes = "sem parâmetros")
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<CustomersDTO> getAllCustomers() {
         return new ResponseEntity<>(new CustomersDTO(customerService.getAllCustomers())
                 , HttpStatus.OK);
     }
 
     @ApiOperation(value = "Retorna um cliente", notes = "usa o primeiro nome")
-    @GetMapping(value = "/{firstname}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{firstname}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<CustomerDTO> getCustomerByFirstName(@PathVariable String firstname) {
         return new ResponseEntity<>(
                 customerService.getCustomerByFirstname(firstname), HttpStatus.OK);
     }
 
     @ApiOperation(value = "Retorna um cliente buscando usando o id")
-    @GetMapping(value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/id/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     @ResponseStatus(HttpStatus.OK)
     public CustomerDTO getCustomerById(@PathVariable Long id) {
         try {
@@ -57,14 +58,15 @@ public class CustomerController {
     }
 
     @ApiOperation(value = "Cria um novo Cliente")
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO) {
         return new ResponseEntity<>(
                 customerService.createNewCustomer(customerDTO), HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "Atualiza os dados de um cliente")
-    @RequestMapping(value = "/id/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/id/{id}", method = RequestMethod.PUT,
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     @ResponseStatus(HttpStatus.OK)
     public CustomerDTO updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
         return customerService.updateCustomer(id, customerDTO);
