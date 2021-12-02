371845985a44f9b259967576dbc3f4c05f797e3a
berkson <berksonx@yahoo.com.br>
2021-12-01 22:17:03 -0300

    Refatorado para multi módulo e configurado o jaxb Closes #32

--- spring5-mvc-rest-app/src/test/java/guru/spring/berkson/domain/CategoryTest.java
+++ spring5-mvc-rest-app/src/test/java/guru/spring/berkson/domain/CategoryTest.java
@@ -3,7 +3,7 @@ package guru.spring.berkson.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by Berkson Ximenes
